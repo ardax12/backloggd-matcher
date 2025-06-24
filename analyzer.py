b176a57e396ae2c7e17a1ee15c382d45bb93290a
Arda Günaydın <149956812+ardax12@users.noreply.github.com>
2025-06-24 15:16:06 +0300

    Add files via upload

--- analyzer.py
+++ analyzer.py
@@ -1,22 +1,55 @@
 import exporter
-import argparse
+from PIL import Image, ImageDraw
 
-parser = argparse.ArgumentParser()
+score = 0
+intersectionLength = 0
 
-parser.add_argument(
-    "user1",
-    type=str,
-)
+def calculateSimilarity(username1,username2):
 
-parser.add_argument(
-    "user2",
-    type=str,
-)
+    user1data = exporter.fetch_all_game_data(f"https://backloggd.com/u/{username1}/games/")
+    user2data = exporter.fetch_all_game_data(f"https://backloggd.com/u/{username2}/games/")
 
-args = parser.parse_args()
+    lookup = {k: v for k, v in user2data}
 
-username1 = args.user1
-username2 = args.user2
+    intersection = [(k, v1, lookup[k]) for k, v1 in user1data if k in lookup]
 
-user1data = exporter.fetch_all_game_data(f"https://backloggd.com/u/{username1}/games/")
-user2data = exporter.fetch_all_game_data(f"https://backloggd.com/u/{username2}/games/")
+    intersectionLength = len(intersection)
+
+    if(intersectionLength ==0):
+        return
+
+    totalLen = len(user1data) + len(user2data) - intersectionLength
+
+    gameOverlap = intersectionLength/totalLen
+
+    gameOverlapScore = gameOverlap * 50
+
+    reviewOverlapScore = 50
+
+    user1TotalReview = 0
+    user2TotalReview = 0
+
+    for tup in intersection:
+        if(tup[1] == 0 or tup[2] == 0):
+            intersection.remove(tup)
+        else:
+            user1TotalReview += tup[1]
+            user2TotalReview += tup[2]
+
+    user1Avg = user1TotalReview/len(intersection)
+    user2Avg = user2TotalReview/len(intersection)
+
+    for tup in intersection:
+        user1Score = tup[1] - user1Avg 
+        user2Score = tup[2] - user2Avg
+        dif = abs(user1Score - user2Score)
+        reviewOverlapScore -= (dif / 4.5) * (50 / len(intersection))
+
+        score = max(0, score)
+
+    score = reviewOverlapScore + gameOverlapScore
+
+
+
+image = Image.new('RGB', (360, 540), color='black')
+draw = ImageDraw.Draw(image)
